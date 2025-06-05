d14de9a6a590c1d3fd8dddded859488238f834db
MartinAle90 <martinale90@gmail.com>
2025-06-05 19:11:07 -0300

    Se agrega el layout, acerca de, yproductos [Daza Martin]

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { Container } from "react-bootstrap";
 import { Routes, Route } from "react-router-dom";
+import Layout from "./assets/components/Layout";
+import Nosotros from "./assets/components/Nosotros";
 import Home from "./assets/components/Home";
+import ErrorPage from "./assets/components/ErrorPage";
 
 function App() {
 
   return (
     <Container>
       <Routes>
-        <Route path="/" element={<Home />}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="nosotros" element={<Nosotros />} />
+          <Route path="*" element={<ErrorPage />} />
         </Route>
       </Routes>
     </Container>
