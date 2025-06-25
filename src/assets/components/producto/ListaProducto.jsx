3368a4a8dfd6ef24b2f592f77752b4a2e4f9556d
Naim-Apaza <naimlautaroapaza@gmail.com>
2025-06-24 22:49:55 -0300

    Agrego buscador por nombre y categoria en ListaProduct y en Favoritos

--- src/assets/components/producto/ListaProducto.jsx
+++ src/assets/components/producto/ListaProducto.jsx
@@ -1,17 +1,26 @@
 import { useState } from "react";
 import { Button, Col, Container, Row, Modal } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
 import { BsStarFill, BsStar } from "react-icons/bs";
 import { useProducts } from "../../context/ProductosContext";
 import ProductCard from "./ProductCard.jsx";
+import { useFilteredProducts } from "../hooks/useFilteredProducts.js";
+import ProductSearch from "./ProductSearch.jsx";
 import "../../css/ListaProductos.css";
 
 // Ya no se usan los props 'productos' y 'setProductos' porque ahora usamos el contexto
 function ListaProducto() {
   const [showModal, setShowModal] = useState(false);
   const [productoSeleccionado, setProductoSeleccionado] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   // Obtenemos los productos y las funciones del contexto
   const { products, toggleFavorite, deleteProduct } = useProducts();
+
+  const filteredProducts = useFilteredProducts(
+    products.filter((p) => p.estado === true),
+    searchTerm,
+  );
 
   const confirmarEliminar = (producto) => {
     setProductoSeleccionado(producto);
@@ -30,19 +39,26 @@ function ListaProducto() {
     <div className="lista-productos">
       <Container className="my-4 text-center">
         <h2 className="mb-4">Lista de productos</h2>
-        <Row>
-          {products // Usar 'products' del contexto
-            .filter((producto) => producto.estado === true)
-            .map((producto) => (
-              <Col md={4} key={producto.id} className="mb-4">
-                <ProductCard
-                  producto={producto}
-                  toggleFavorite={toggleFavorite}
-                  confirmarEliminar={confirmarEliminar}
-                />
-              </Col>
-            ))}
-        </Row>
+        <ProductSearch searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+        {filteredProducts.length === 0 ? (
+          <Alert className="text-center mt-4" variant="info">
+            No hay productos que coincidan con tu búsqueda.
+          </Alert>
+        ) : (
+          <Row>
+            {filteredProducts
+              .filter((producto) => producto.estado === true)
+              .map((producto) => (
+                <Col md={4} key={producto.id} className="mb-4">
+                  <ProductCard
+                    producto={producto}
+                    toggleFavorite={toggleFavorite}
+                    confirmarEliminar={confirmarEliminar}
+                  />
+                </Col>
+              ))}
+          </Row>
+        )}
 
         <Modal show={showModal} onHide={() => setShowModal(false)} centered>
           <Modal.Header closeButton>
--- src/assets/pages/Favoritos.jsx
+++ src/assets/pages/Favoritos.jsx
@@ -3,39 +3,42 @@ import { BsStarFill, BsStar } from "react-icons/bs";
 import { useProducts } from "../context/ProductosContext";
 import Alert from "react-bootstrap/Alert";
 import ProductCard from "../components/producto/ProductCard.jsx";
-
+import { useFilteredProducts } from "../components/hooks/useFilteredProducts.js";
+import ProductSearch from "../components/producto/ProductSearch.jsx";
+import { useState } from "react";
 import "../css/Favoritos.css";
 
 function Favoritos() {
   // Ya no necesitas recibir 'productos' y 'setProductos' como props aquí
   // Usar el hook personalizado para obtener los datos y funciones del contexto
-  const { products, toggleFavorite, favorites } = useProducts();
+  const { deleteProduct, toggleFavorite, favorites } = useProducts();
+  const [searchTerm, setSearchTerm] = useState("");
 
-  if (favorites.length === 0) {
-    return (
-      <Alert className="text-center mt-5" key="info" variant="info">
-        No hay productos favoritos.
-      </Alert>
-    );
-  }
+  const filteredFavorites = useFilteredProducts(favorites, searchTerm);
 
   return (
     <Container className="my-4 text-center">
       <h2 className="mb-4">Lista de productos Favoritos</h2>
-      <Row>
-        {favorites.map(
-          (
-            producto, // Iterar sobre 'favoritos'
-          ) => (
+
+      <ProductSearch searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+
+      {filteredFavorites.length === 0 ? (
+        <Alert className="text-center mt-5" key="info" variant="info">
+          No hay productos favoritos.
+        </Alert>
+      ) : (
+        <Row>
+          {filteredFavorites.map((producto) => (
             <Col md={4} key={producto.id} className="mb-4">
               <ProductCard
                 producto={producto}
                 toggleFavorite={toggleFavorite}
+                deleteProduct={deleteProduct}
               />
             </Col>
-          ),
-        )}
-      </Row>
+          ))}
+        </Row>
+      )}
     </Container>
   );
 }
